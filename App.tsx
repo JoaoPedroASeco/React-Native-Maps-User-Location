ac66cbf2df9e762b1b825728cb684e6e22a62ec2
DevJpedro <dev.jpedro@gmail.com>
2023-10-30 22:25:53 -0300

    Finalizado, pendente a API Key do Google maps

--- App.tsx
+++ App.tsx
@@ -1,20 +1,112 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState, useRef } from "react";
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+  LocationObject,
+  watchPositionAsync,
+  LocationAccuracy,
+  reverseGeocodeAsync,
+} from "expo-location";
+import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
+  const mapRef = useRef<any>(null);
+  const [location, setLocation] = useState<LocationObject | null>(null);
+  const [address, setAddress] = useState<any>(null);
+
+  const requestLocationPermission = async () => {
+    const { granted } = await requestForegroundPermissionsAsync();
+
+    if (granted) {
+      const currentPosition = await getCurrentPositionAsync();
+      setLocation(currentPosition);
+    }
+  };
+
+  useEffect(() => {
+    requestLocationPermission();
+  }, []);
+  useEffect(() => {
+    watchPositionAsync(
+      {
+        accuracy: LocationAccuracy.Highest,
+        timeInterval: 1000,
+        distanceInterval: 1,
+        mayShowUserSettingsDialog: true,
+      },
+      (response) => {
+        setLocation(response);
+        mapRef.current?.animateCamera({
+          center: response.coords,
+        });
+
+        const handleGetAddressInfo = async () => {
+          const addressInfo = await reverseGeocodeAsync({
+            latitude: response.coords.latitude,
+            longitude: response.coords.longitude,
+          });
+          setAddress(addressInfo);
+        };
+        handleGetAddressInfo();
+      }
+    );
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {location && mapRef && (
+        <>
+          <GooglePlacesAutocomplete
+            placeholder="Search for a location"
+            fetchDetails={true}
+            onPress={(data, details: any) => {
+              // Handle the selected location data here
+              console.log(data, details);
+              // Update the map region with the selected location
+              // For example:
+              const { lat, lng } = details.geometry.location;
+              mapRef?.current.animateToRegion({
+                latitude: lat,
+                longitude: lng,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005,
+              });
+            }}
+            query={{
+              key: "YOUR_GOOGLE_PLACES_API_KEY",
+              language: "pt-BR",
+            }}
+          />
+          <MapView
+            ref={mapRef}
+            style={styles.map}
+            initialRegion={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+              latitudeDelta: 0.005,
+              longitudeDelta: 0.005,
+            }}
+          >
+            <Marker
+              coordinate={{
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+              }}
+            />
+          </MapView>
+        </>
+      )}
+      <View>
+        {address && (
+          <>
+            <Text>{address.city}</Text>
+            <Text>{address.country}</Text>
+          </>
+        )}
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0d0d12",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: "80%",
+    height: "50%",
+  },
+});
